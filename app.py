2c6f004e4a219e2720669f5fed6ce975a47eb948
yohhei someya <youhei19880130@yahoo.co.jp>
2021-02-08 20:14:10 +0900

    demo shuffle

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import random
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html", title="ロールパンダ")
+    filenames = ['image_1.jpg', 'image_2.jpg']
+    app.logger.debug(filenames)
+    random.shuffle(filenames)
+    app.logger.debug(filenames)
+    filename = filenames[0]
+    return render_template("index.html", title="ロールパンダ", file=filename)
 
 if __name__ == "__main__":
     app.run()
