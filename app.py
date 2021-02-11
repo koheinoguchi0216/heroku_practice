815fa650c0c1fd318165011a334c527182f71ed2
koheinoguchi <rikichan0216@gmail.com>
2021-02-11 09:54:48 +0900

    update

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+import random
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 
 @app.route("/")
 def hello():
-    return render_template("index.html", title="ロールパンナちゃん")
+    filenames = ['image_1.jpg','image_2.jpg','image_3.jpg','image_4.jpg','image_5.jpg']
+    app.logger.debug(filenames)
+    random.shuffle(filenames)
+    app.logger.debug(filenames)
+    filename = filenames[0]
+    return render_template("index.html", title='aaa', file=filename)
+
 
 if __name__ == "__main__":
     app.run()
