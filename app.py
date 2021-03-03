4c3f42ef849d2676f0cd7eee1e5c8c061d2cf43d
koheinoguchi <rikichan0216@gmail.com>
2021-03-03 13:46:44 +0900

    edit app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """flask appの初期化を行い、flask appオブジェクトの実体を持つ"""
 from flask import Flask
-import database
-# from flask_sample.database
+
+from flask_sample.database import init_db
 
 
 def create_app():
