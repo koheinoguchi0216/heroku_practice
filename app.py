dde1b5a64a7154ac4aab6b79af562b6c52ab89eb
koheinoguchi <rikichan0216@gmail.com>
2021-03-21 20:52:33 +0900

    app.py,database.pyを修正

--- app.py
+++ app.py
@@ -12,16 +12,10 @@ db.init_app(app)
 
 @app.route("/")
 def hello():
-    # names = [['image_1.jpg','ロールパンナ'],['image_2.jpg','アンパンマン'],['image_3.jpg','ばいきんまん'],['image_4.jpg','ダダンダン'],['image_5.jpg','カレーパンマン']]
     names = db.session.query(Character.name, Character.image_path).all()
-    app.logger.debug(names)
-    # app.logger.debug(names)
     random.shuffle(names)
-    # app.logger.debug(filenames)
     filename = names[0][1]
-    app.logger.debug("filename: " + filename)
     character_name = names[0][0]
-    app.logger.debug("character_name: " + character_name)
 
     return render_template("index.html", title=character_name, file=filename)
 
--- database.py
+++ database.py
@@ -1,10 +1,8 @@
 """FlaskアプリがSQLAlchemyを使えるようにするための初期化"""
 from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate  # 追加 （…一旦コメントアウトします）
 
 db = SQLAlchemy()
 
 
 def init_db(app):
     db.init_app(app)
-    # Migrate(app, db)  # 追加 （…一旦コメントアウトします）
