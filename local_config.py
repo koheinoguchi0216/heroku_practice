54cbd235b4d2b055e102b1b2eac32e849a3f1b27
koheinoguchi <rikichan0216@gmail.com>
2021-04-06 19:43:02 +0900

    Remove local_config.py

--- local_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = True
-
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/mydb?charset=utf8'.format(**{
-    'user': os.getenv('DB_USER', 'rikizou'),
-    'password': os.getenv('DB_PASSWORD', '{Rikizou105d}'),
-    'host': os.getenv('DB_HOST', 'localhost'),
-})
