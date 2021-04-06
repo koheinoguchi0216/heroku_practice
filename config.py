3f070ac1b27329e445525de1eba329b46282c263
koheinoguchi <rikichan0216@gmail.com>
2021-04-06 19:37:33 +0900

    Remove unused code from config.py

--- config.py
+++ config.py
@@ -5,23 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Flask
-DEBUG = False
+DEBUG = True
 
 class DevelopmentConfig:
 
-    if not DEBUG:
-
-        # Flask
-        DEBUG = True
-
-        # SQLAlchemy
-        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-            'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
-            'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
-            'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
-            'db_name': os.getenv('DB_DATABASE', os.environ['DB_NAME']),
-        })
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-        SQLALCHEMY_ECHO = False
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+        'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
+        'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+        'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+        'db_name': os.getenv('DB_DATABASE', os.environ['DB_NAME']),
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
 
 Config = DevelopmentConfig
