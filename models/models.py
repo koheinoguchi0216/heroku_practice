87df69c30d3884db252d6a33d980690c1022abf9
koheinoguchi <rikichan0216@gmail.com>
2021-05-01 09:53:39 +0900

    静的コード解析ツールを入れる

--- models/models.py
+++ models/models.py
@@ -1,16 +1,19 @@
 from datetime import datetime
+
 from database import db
+
 
 class Character(db.Model):
 
-    __tablename__ = 'characters'
+    __tablename__ = "characters"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     image_path = db.Column(db.String(255), nullable=False)
 
+
 class Test(db.Model):
-    __tablename__ = 'tests'
+    __tablename__ = "tests"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
