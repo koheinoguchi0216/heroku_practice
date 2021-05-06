95a62c2053406465d121079962811260a4e22a90
koheinoguchi <rikichan0216@gmail.com>
2021-05-06 20:49:09 +0900

    pipenv run black

--- config.py
+++ config.py
@@ -2,21 +2,28 @@
 import os
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Flask
 DEBUG = True
 
+
 class DevelopmentConfig:
 
     # SQLAlchemy
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-        'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
-        'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
-        'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
-        'db_name': os.getenv('DB_DATABASE', os.environ['DB_NAME']),
-    })
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8".format(
+            **{
+                "user": os.getenv("DB_USER", os.environ["DB_USERNAME"]),
+                "password": os.getenv("DB_PASSWORD", os.environ["DB_PASSWORD"]),
+                "host": os.getenv("DB_HOST", os.environ["DB_HOSTNAME"]),
+                "db_name": os.getenv("DB_DATABASE", os.environ["DB_NAME"]),
+            }
+        )
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
+
 
 Config = DevelopmentConfig
