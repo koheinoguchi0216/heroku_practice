982d9ccc5f3208ab6312aef8637576ce6c6679de
koheinoguchi <rikichan0216@gmail.com>
2021-05-07 20:47:13 +0900

    コンフリクト解消

--- app.py
+++ app.py
@@ -5,20 +5,19 @@ from models import Character
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config.from_object('config.Config')
+app.config["DEBUG"] = True
+app.config.from_object("config.Config")
 
 db.init_app(app)
 Migrate(app, db)
 
-@app.route("/")
 
+@app.route("/")
 def top():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
 @app.route("/images")
-
 def hello():
     names = db.session.query(Character.name, Character.image_path).all()
     random.shuffle(names)
@@ -26,6 +25,7 @@ def hello():
     character_name = names[0][0]
 
     return render_template("index.html", title=character_name, file=filename)
+
 
 if __name__ == "__main__":
     app.run()
